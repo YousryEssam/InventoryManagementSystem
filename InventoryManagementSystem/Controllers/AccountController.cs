7fedbde1bebee65ddbe7725f3c5528c3156c04f1
YousryEssam <youssry.essam2111@gmail.com>
2025-05-02 15:15:54 +0300

    Add AccountController
    Added AccountController class for user authentication and authorization
    with necessary dependencies for configuration, user management,
    and role management.

---
+++ InventoryManagementSystem/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole<int>> _roleManager;
+
+        public AccountController(IConfiguration configuration, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole<int>> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+    }
+}
