fbb87f233f76662b3694efd35bbab65bfc2deb97
YousryEssam <youssry.essam2111@gmail.com>
2025-05-03 15:49:27 +0300

    Add ReportController for admin API access
    Introduces a new `ReportController` in the
    `InventoryManagementSystem.Controllers` namespace.
    This controller is set up as an API controller,
    requires "Admin" role authorization, and is
    routed under "api/[controller]". The constructor
    accepts an `IMediator` instance to initialize
    the base `BaseAPIController`.

---
+++ InventoryManagementSystem/Controllers/ReportController.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManagementSystem.Controllers
+{
+    [ApiController]
+    [Authorize(Roles ="Admin")]
+    [Route("api/[controller]")]
+    public class ReportController: BaseAPIController
+    {
+        public ReportController(IMediator mediator) : base(mediator) { }
+    }
+}
